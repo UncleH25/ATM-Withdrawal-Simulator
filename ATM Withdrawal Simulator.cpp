83368a918b072dc99f64e456bae700bc47e585d5
UncleH25 <oneilljarryd@gmail.com>
2025-05-30 19:27:05 +0200

    Added initial input to prompt the user to enter their current balance, and the to validate the balance using assert for if balance is greater than or equal to 0.0.

--- ATM Withdrawal Simulator.cpp
+++ ATM Withdrawal Simulator.cpp
@@ -12,6 +12,13 @@ int main()
     double fee = 0.0;
     char choice;
 
+    //Input
+	cout << "Enter your current balance: R";
+	cin >> balance;
+
+	//Validate balance input
+	assert(balance >= 0.0);
+
 
     return 0;
 }
