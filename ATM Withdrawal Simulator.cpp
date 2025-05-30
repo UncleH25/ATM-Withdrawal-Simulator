e645b2cc848af2346891a68be225b4a91d96312c
UncleH25 <oneilljarryd@gmail.com>
2025-05-30 19:42:11 +0200

    Added a do-while loop for displaying the atm simulation while choice equal 'y' or 'Y'. Inside the loop, the user's current balance is displayed, with the user being prompted to input a withdrawal amount or exit (Input of 0 exits the loop). If the withdrawal is greater than the balance, an error is displayed. If the withdrawal amount is less than the balance, the withdrawal amount is deducted from the balance, then the balance is displayed. Then an if-else if-else block calculates the fee based on a certain amount withdrawn. Then the output of the withdrawal summary is displayed, then the user is prompted to exit, or make another withdrawal.

--- ATM Withdrawal Simulator.cpp
+++ ATM Withdrawal Simulator.cpp
@@ -19,6 +19,67 @@ int main()
 	//Validate balance input
 	assert(balance >= 0.0);
 
+	//Do-while loop for ATM withdrawal simulation while choice equals 'y' or 'Y'
+    do 
+    {
+		cout << "\nATM Withdrawal Simulator\n";
+        cout << "-------------------------\n";
+		//Display current balance
+        cout << "Current Balance: R" << fixed << setprecision(2) << balance << endl;
+		//Input withdrawal amount
+        cout << "Enter withdrawal amount (or 0 to exit): R";
+        cin >> withdrawal;
+
+		//If withdrawal is 0, exit the loop
+        if(withdrawal == 0) 
+        {
+            cout << "Exiting the ATM Withdrawal Simulator.\n";
+            break;
+		}
+
+		//If withdrawal is greater than balance, display error
+        if (withdrawal > balance)
+        {
+            cout << "ERROR INSUFFICIENT FUNDS\n";
+        }
+        //Else balance greater than or equal to withdrawal
+        else
+        {
+			//Deduct withdrawal amount from balance
+            balance -= withdrawal;
+
+			cout << fixed << setprecision(2);
+
+			//Display new balance after withdrawal
+			cout << "Withdrawal Successful. New Balance: R" << balance << endl;
+
+            //Calculate fee based on withdrawal amount
+            if (withdrawal < 100.0)
+            {
+                fee = 2.0;
+            }
+            else if (withdrawal < 200.0)
+            {
+                fee = 3.0;
+            }
+            else
+            {
+                fee = 5.0;
+			}
+		} 
+
+		//Output of withdrawal summary
+        cout << "Withdrawal Amount: R" << withdrawal << endl;
+        cout << "Transaction Fee: R" << fee << endl;
+		cout << "Total Deducted: R" << (withdrawal + fee) << endl;
+		cout << "Remaining Balance: R" << balance << endl;
+
+		//Prompt the user to continue or exit
+        cout << "Do you want to make another withdrawal? (y/n): ";
+		cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
 
     return 0;
 }
